3a6336c9d67e6c9239c96e09dfe6d333011b28b4
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-11-02 12:29:45 +0000

    Publish

--- modules/generator/src/helpers/template-generator.helper.ts
+++ modules/generator/src/helpers/template-generator.helper.ts
@@ -3,5 +3,7 @@ export function $templateGenerator(template: string, scope: any): string {
         template = template.replace(new RegExp(`{{${property}}}`, 'g'), scope[property]);
     });
 
+    template.replace(/<ng-content(\s.+?>)?<\/ng-content>/g, `<p>Hello world, this is the example markup.</p>`);
+
     return template;
 }
