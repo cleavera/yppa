e2564f76a53a06a37a6c25d36da2ff90fff5eaad
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-11-15 16:29:53 +0000

    Make better use of yargs

--- modules/cli/src/index.ts
+++ modules/cli/src/index.ts
@@ -5,11 +5,10 @@ import { Component, Project } from '@yppa/parser';
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
-const { src, out }: { src: string, out: string } = require('yargs').argv;
-
-if (!src || !out) {
-    throw new Error('Missing required argument "src" or "out"');
-}
+const { src, out }: { src: string, out: string } = require('yargs')
+    .usage('Usage $0 --src="[source-glob]" --out="[out-dir]"')
+    .demandOption(['src', 'out'])
+    .argv;
 
 Project.FromGlob(src).components.forEach((component: Component) => {
     if (!existsSync(out)) {
